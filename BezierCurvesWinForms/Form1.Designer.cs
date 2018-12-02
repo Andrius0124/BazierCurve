4367ed3af65b7eecc863b899e62928514ca9bb71
Andrius <andrius0124@gmail.com>
2018-12-02 20:50:47 +0000

    .NET Core v3 WinForms app
    brezier curve application with .NetCoreV3 inside.

---
+++ BezierCurvesWinForms/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿using System;
+
+namespace BezierCurves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Curve = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.MyBezierCurve = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Curve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyBezierCurve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Curve
+            // 
+            this.Curve.Location = new System.Drawing.Point(12, 29);
+            this.Curve.Name = "Curve";
+            this.Curve.Size = new System.Drawing.Size(454, 494);
+            this.Curve.TabIndex = 0;
+            this.Curve.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.MyBezierCurve.Location = new System.Drawing.Point(472, 29);
+            this.MyBezierCurve.Name = "MyBezierCurve";
+            this.MyBezierCurve.Size = new System.Drawing.Size(454, 494);
+            this.MyBezierCurve.TabIndex = 1;
+            this.MyBezierCurve.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(970, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(965, 449);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Intervals";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1177, 449);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1021, 449);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(150, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.ValueChanged += new EventHandler(this.trackBar1_LocationChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(920, 402);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "End point control";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(950, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "End point";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(920, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Start point control";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(950, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Start point";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1008, 306);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 10;
+            
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1045, 306);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1045, 332);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1008, 332);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(31, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1045, 360);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(30, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1008, 360);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(31, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1045, 395);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(30, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1008, 395);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(31, 20);
+            this.textBox9.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 761);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyBezierCurve);
+            this.Controls.Add(this.Curve);
+            this.Name = "Form1";
+            this.Text = "Bezier Curve";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Curve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyBezierCurve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Curve;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox MyBezierCurve;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ BezierCurvesWinForms/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BZC = BezierCurve.Logic.BezierCurve;
+
+namespace BezierCurves
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 100;
+            
+            Point p1 = new Point(25,25);
+            Point p2 = new Point(300,25);
+            Point p3 = new Point(25,300);
+            Point p4 = new Point(300, 300);
+            List<Point> p = new List<Point>(){p1,p2,p3,p4};
+            InitBezierCurve(Curve, p);
+
+
+            textBox1.Text = "1";
+
+        }
+
+        private void InitCustomBezierCurve(PictureBox pic)
+        {
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            BZC customCurve = new BZC();
+            customCurve.Curve.StartPoint = new Vector2(Convert.ToSingle(textBox2.Text),Convert.ToSingle(textBox3.Text));
+            customCurve.Curve.StartPontControl = new Vector2(Convert.ToSingle(textBox8.Text),Convert.ToSingle(textBox9.Text));
+            customCurve.Curve.EndPoint = new Vector2(Convert.ToSingle(textBox6.Text),Convert.ToSingle(textBox7.Text));
+            customCurve.Curve.EndPointControl = new Vector2(Convert.ToSingle(textBox4.Text),Convert.ToSingle(textBox5.Text));
+            customCurve.Curve.NumberOfIntervals = (uint)trackBar1.Value;
+            customCurve.GetAllPointsOnCurve();
+            
+
+            Bitmap DrawArea = new Bitmap(pic.Size.Width, pic.Size.Height);
+            Graphics g = Graphics.FromImage(DrawArea);
+            pic.BorderStyle = BorderStyle.FixedSingle;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            
+            Pen bl = new System.Drawing.Pen(System.Drawing.Color.Black);
+            Pen red = new System.Drawing.Pen(System.Drawing.Color.Red);
+            
+            Point p1 = new Point((int)customCurve.Curve.StartPoint.X,(int)customCurve.Curve.StartPoint.Y);
+            Point p2 = new Point((int)customCurve.Curve.StartPontControl.X,(int)customCurve.Curve.StartPontControl.Y);
+            Point p3 = new Point((int)customCurve.Curve.EndPoint.X,(int)customCurve.Curve.EndPoint.Y);
+            Point p4 = new Point((int)customCurve.Curve.EndPointControl.X,(int)customCurve.Curve.EndPointControl.Y);
+            
+            List<Point> originalPoints = new List<Point>(){p1,p2,p3,p4};
+            List<Point> points = new List<Point>(){p1,p2,p3,p4};
+            
+            float scaleX = (pic.Size.Width/2-10)/(float)GreatestX(points);
+            float scaleY = (pic.Size.Height/2-10)/(float)GreatestY(points);
+            
+            for (int i = 0; i < points.Count; i++)
+            {
+                points[i] = new Point(pic.Size.Width / 2 + (int)(points[i].X * scaleX),(pic.Size.Height / 2 - (int)(points[i].Y * scaleY)));
+            }
+            Point xStart = new Point(0,pic.Size.Height/2);
+            Point xEnd = new Point(pic.Size.Width,pic.Size.Height/2);
+            Point yStart = new Point(pic.Size.Width/2,0); 
+            Point yEnd = new Point(pic.Size.Width/2,pic.Size.Height); 
+            
+            g.DrawLine(Pens.Black,xStart,xEnd);
+            g.DrawLine(Pens.Black,yStart,yEnd);
+            
+
+            for (int i = 0; i < customCurve.Curve.PointsOnCurve.Count; i++)
+            {
+                int x = pic.Size.Width / 2 + (int) (customCurve.Curve.PointsOnCurve[i].X * scaleX);
+                int y = pic.Size.Height / 2 - (int) (customCurve.Curve.PointsOnCurve[i].Y * scaleY);
+                
+                g.DrawRectangle(red,x-1,y-1,2,2);
+                if (i != customCurve.Curve.PointsOnCurve.Count - 1)
+                {
+                    int x1 = pic.Size.Width / 2 + (int) (customCurve.Curve.PointsOnCurve[i+1].X * scaleX);
+                    int y1 = pic.Size.Height / 2 - (int) (customCurve.Curve.PointsOnCurve[i+1].Y * scaleY);
+                    g.DrawLine(red,x,y,x1,y1);
+                }
+            }
+
+            StringFormat stringFormat = new StringFormat {Alignment = StringAlignment.Far};
+            for (int i = 0; i < points.Count; i++)
+            {
+                g.DrawRectangle(bl,points[i].X-1,points[i].Y-1,2,2);
+                if (points[i].X > pic.Size.Width - 20)
+                {
+                    string coordinates = string.Format("{0}:{1}", originalPoints[i].X, originalPoints[i].Y);
+                    g.DrawString(coordinates,new Font("Arial", 9),  SystemBrushes.WindowText, new Point(points[i].X,points[i].Y),stringFormat );
+                }
+                else
+                {
+                    g.DrawString(string.Format("{0}:{1}",originalPoints[i].X,originalPoints[i].Y),new Font("Arial", 9),  SystemBrushes.WindowText, points[i]);
+                }
+            }
+
+            pic.Image = DrawArea;
+        }
+
+        private void InitBezierCurve(PictureBox pic, List<Point> points)
+        {
+            List<Point> originalPoints = new List<Point>(points);
+           
+            float scaleX = (pic.Size.Width/2-10)/(float)GreatestX(points);
+            float scaleY = (pic.Size.Height/2-10)/(float)GreatestY(points);
+            
+            Bitmap DrawArea = new Bitmap(pic.Size.Width, pic.Size.Height);
+            Graphics g = Graphics.FromImage(DrawArea);
+            pic.BorderStyle = BorderStyle.FixedSingle;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            
+            Pen bl = new System.Drawing.Pen(System.Drawing.Color.Black);
+            Pen red = new System.Drawing.Pen(System.Drawing.Color.Red);
+            
+            Point xStart = new Point(0,pic.Size.Height/2);
+            Point xEnd = new Point(pic.Size.Width,pic.Size.Height/2);
+            Point yStart = new Point(pic.Size.Width/2,0); 
+            Point yEnd = new Point(pic.Size.Width/2,pic.Size.Height); 
+
+            g.DrawLine(Pens.Black,xStart,xEnd);
+            g.DrawLine(Pens.Black,yStart,yEnd);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                points[i] = new Point(pic.Size.Width / 2 + (int)(points[i].X * scaleX),(pic.Size.Height / 2 - (int)(points[i].Y * scaleY)));
+            }
+
+            g.DrawBezier(red,points[0],points[1],points[2],points[3]);
+
+            StringFormat stringFormat = new StringFormat {Alignment = StringAlignment.Far};
+            for (int i = 0; i < points.Count; i++)
+            {
+                g.DrawRectangle(bl,points[i].X-1,points[i].Y-1,2,2);
+                if (points[i].X > pic.Size.Width - 20)
+                {
+                    string coordinates = string.Format("{0}:{1}", originalPoints[i].X, originalPoints[i].Y);
+                    g.DrawString(coordinates,new Font("Arial", 9),  SystemBrushes.WindowText, new Point(points[i].X,points[i].Y),stringFormat );
+                }
+                else
+                {
+                    g.DrawString(string.Format("{0}:{1}",originalPoints[i].X,originalPoints[i].Y),new Font("Arial", 9),  SystemBrushes.WindowText, points[i]);
+                }
+            }
+
+            
+            pic.Image = DrawArea;
+        }
+
+        private int GreatestX(List<Point> points)
+        {
+            var greatestX = 0;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].X  > greatestX)
+                {
+                    greatestX = points[i].X;
+                }
+            }
+
+            return greatestX;
+        }
+        private int GreatestY(List<Point> points)
+        {
+            var greatestY = 0;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].Y  > greatestY)
+                {
+                    greatestY = points[i].Y;
+                }
+            }
+
+            return greatestY;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_LocationChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitCustomBezierCurve(MyBezierCurve);
+        }
+    }
+}
